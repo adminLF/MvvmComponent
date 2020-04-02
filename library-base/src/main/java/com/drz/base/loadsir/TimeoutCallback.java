fcea9f126b46e1b8f6b521260b78be66d62d2a47
linfeng <amingafeng@163.com>
2020-04-02 14:26:29 +0800

    Initial commit

---
+++ library-base/src/main/java/com/drz/base/loadsir/TimeoutCallback.java
@@ -0,0 +1,21 @@
+package com.drz.base.loadsir;
+
+import com.kingja.loadsir.callback.Callback;
+import com.drz.base.R;
+
+/**
+ * 应用模块:
+ * <p>
+ * 类描述: 网络超时
+ * <p>
+ *
+ */
+public class TimeoutCallback extends Callback
+{
+    @Override
+    protected int onCreateView()
+    {
+        return R.layout.base_layout_timeout;
+    }
+    
+}
---
+++ library-common/src/main/java/com/drz/common/services/IAppInfoService.java
@@ -0,0 +1,24 @@
+package com.drz.common.services;
+
+import com.alibaba.android.arouter.facade.template.IProvider;
+
+/**
+ * 应用模块: services
+ * <p>
+ * 类描述: app相关信息
+ * <p>
+ *
+ */
+public interface IAppInfoService extends IProvider
+{
+    String APP_INFO_SERVICE_NAME = "app_info_service";
+    
+    String getApplicationName();
+    
+    String getApplicationVersionName();
+    
+    String getApplicationVersionCode();
+    
+    boolean getApplicationDebug();
+
+}
